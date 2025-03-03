7cb402b084ba818df16fd95ddc158c4cc18f534f
chael-in <cofls3511@gmail.com>
2025-03-04 03:54:53 +0900

    refactor: 스타일드 컴포넌트 방식으로 코드 리팩토링

--- src/Pages/BookDetail.js
+++ src/Pages/BookDetail.js
@@ -1,6 +1,23 @@
 import {useEffect, useState} from 'react';
 import {useLocation, useNavigate} from 'react-router-dom';
-import './BookDetail.css';
+import {
+    BookDetailWrapper,
+    SearchBar,
+    Logo,
+    SearchPlaceholder,
+    BookDetailContainer,
+    BookInfoBox,
+    BookImage,
+    BookTitle,
+    BookAuthor,
+    BookDescription,
+    ButtonGroup,
+    Button,
+    IconWrapper,
+    ButtonText,
+    BottomNav,
+    NavButton,
+} from './BookShelfStyled'
 
 const BookDetail = () => {
     const navigate = useNavigate();
@@ -43,62 +60,62 @@ const BookDetail = () => {
     }
 
     return (
-        <div className="book-detail-wrapper">
+        <BookDetailWrapper>
             {/* 검색창 */}
-            <div className="search-bar">
-                <img src="/images/Logo.svg" alt="로고" className="logo" />
-                <div onClick={handleSearchClick} className="search-placeholder">
+            <SearchBar>
+                <Logo src="/images/Logo.svg" alt="로고" />
+                <SearchPlaceholder onClick={handleSearchClick}>
                     <span></span>
                     <img src="/images/SearchIcon.svg" alt="검색" className="icon" />
-                </div>
-            </div>
+                </SearchPlaceholder>
+            </SearchBar>
 
             {/* 메인 콘텐츠 */}
             {book && (
-                <div className="book-detail-container">
+                <BookDetailContainer>
                     {/* 책 정보 박스 */}
-                    <div className="book-info-box">
-                        <img src={book.image} alt={book.title} className="book-image"/>
-                        <h2 className="book-title">{book.title}</h2>
-                        <p className="book-author"><strong>저자 정보</strong> {book.author}</p>
-                        <p className="book-description"><strong>소개</strong> {book.description}</p>
-                    </div>
+                    <BookInfoBox>
+                        <BookImage src={book.image} alt={book.title} />
+                        <BookTitle>{book.title}</BookTitle>
+                        <BookAuthor><strong>저자 정보</strong> {book.author}</BookAuthor>
+                        <BookDescription><strong>소개</strong> {book.description}</BookDescription>
+                    </BookInfoBox>
 
                     {/* 버튼 */}
-                    <div className="button-group">
-                        <button className="wishlist-btn" onClick={handleWishClick}>
-                            <div className="icon-wrapper">
+                    <ButtonGroup>
+                        <Button onClick={handleWishClick}>
+                            <IconWrapper>
                                 <img src="/images/HeartIcon.svg" alt="찜하기"/>
-                            </div>
-                            <span className="button-text">책 찜하기</span>
-                        </button>
+                            </IconWrapper>
+                            <ButtonText>책 찜하기</ButtonText>
+                        </Button>
 
-                        <button className="register-btn" onClick={handleRegisterClick}>
-                            <div className="icon-wrapper">
+                        <Button onClick={handleRegisterClick}>
+                            <IconWrapper>
                                 <img src="/images/RegisterIcon.svg" alt="등록하기"/>
-                            </div>
-                            <span className="button-text">책 등록하기</span>
-                        </button>
-                    </div>
-                </div>
+                            </IconWrapper>
+                            <ButtonText>책 등록하기</ButtonText>
+                        </Button>
+                    </ButtonGroup>
+                </BookDetailContainer>
             )}
 
             {/* 하단 네비게이션 */}
-            <nav className="bottom-nav">
-                <button onClick={() => navigate("/")} className="nav-button active">
+            <BottomNav>
+                <NavButton onClick={() => navigate("/")}>
                     <img src="/images/HomeIcon.svg" alt="홈"/>
                     <span>홈</span>
-                </button>
-                <button onClick={() => navigate("/to-read")} className="nav-button">
+                </NavButton>
+                <NavButton onClick={() => navigate("/to-read")}>
                     <img src="/images/BookcaseIcon.svg" alt="책장"/>
                     <span>책장</span>
-                </button>
-                <button onClick={() => navigate("/note")} className="nav-button">
+                </NavButton>
+                <NavButton onClick={() => navigate("/note")} className="nav-button">
                     <img src="/images/NoteIcon.svg" alt="노트"/>
                     <span>노트</span>
-                </button>
-            </nav>
-        </div>
+                </NavButton>
+            </BottomNav>
+        </BookDetailWrapper>
     );
 };
 
