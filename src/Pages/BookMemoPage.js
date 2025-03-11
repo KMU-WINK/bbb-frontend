9ff6852d1343bd6874a267edb32ab2fa38e07924
arirang030 <arirang030@kookmin.ac.kr>
2025-03-11 18:11:47 +0900

    [FIX] 메모 페이지 성공!

--- src/Pages/BookMemoPage.js
+++ src/Pages/BookMemoPage.js
@@ -1,21 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
 import styled from "styled-components";
 import { FiEdit2 } from "react-icons/fi";
 import { useNavigate } from "react-router-dom";
 
 const BookMemoPage = () => {
+  const [memos, setMemos] = useState([]);
   const navigate = useNavigate();
 
-  const memos = [
-    { id: 1, bookCover: "", text: "죽고싶지만 떡볶이는 먹고싶어", date: "2024. 11. 24" },
-    { id: 2, bookCover: "", text: "곰돌이 푸, 행복한 일은 매일 있어", date: "2024. 12. 11" },
-    { id: 3, bookCover: "", text: "선형대수", date: "2025. 1. 30" },
-    { id: 2, bookCover: "", text: "자료구조", date: "2024. 11. 24"},
-    { id: 2, bookCover: "", text: "C++", date: "2024. 11. 24"},
-  ];
+  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiZW1haWwiOiJhc0BhLmNvbSIsImlhdCI6MTc0MTY4MzU1NiwiZXhwIjoxNzQxNjg3MTU2fQ.fyao79oCR6qkIlz_Zsincgbtcp_Gr1EuTYoZ5xkExNE';
+
+  useEffect(() => {
+    axios.get(`http://${process.env.REACT_APP_API_URL}:3000/finishlist`, {
+      headers: {
+        'Cache-Control': 'no-cache',
+        Authorization: `Bearer ${token}`
+      }
+    })
+    .then((res) => {
+      const readBookList = res.data.data || [];
+      console.log(readBookList);
+      console.log(readBookList.map((book) => book.title));
+      setMemos(readBookList);
+    })
+    .catch((err) => {
+      console.error("/finishlist API 호출 중 에러 발생", err);
+    })
+  }, []);
 
   return (
     <Container>
+      {/* 검색바 */}
       <SearchBar>
         <Logo src="/logo.png" alt="로고" />
         <SearchPlaceholder>
@@ -23,23 +38,30 @@ const BookMemoPage = () => {
           <img className="icon" src="/searchicon.png" alt="검색 아이콘" />
         </SearchPlaceholder>
       </SearchBar>
+
+      {/* 메모한 책 리스트 */}
       <MemoList>
-        {memos.map((memo) => (
-          <MemoItem key={memo.id}>
-            <BookCover />
-            <MemoContent>
-              <MemoText>{memo.text}</MemoText>
-              <MemoInfo>
+        {memos && memos.length > 0 ? (
+          memos.map((memo) => (
+            <MemoItem key={memo.id}>
+              <BookCover />
+              <MemoContent>
+                <MemoText>{memo.title}</MemoText>
+                <MemoInfo>
                 <EditButton onClick={() => navigate("/book-record")}>
                   <FiEdit2 size={14} />
                 </EditButton>
-                <span>1개</span>
-              </MemoInfo>
-              <MemoDate>{memo.date}</MemoDate>
-            </MemoContent>
-          </MemoItem>
-        ))}
+                </MemoInfo>
+                <MemoDate>{memo.updatedAt}</MemoDate>
+              </MemoContent>
+            </MemoItem>
+          ))
+        ) : (
+          <EmptyMessage>현재 작성한 메모가 없습니다.</EmptyMessage>
+        )}
       </MemoList>
+
+      {/* 하단바 */}
       <BottomNav>
         <NavButton onClick={() => navigate("/")}>
           <img src="/images/HomeIcon.svg" alt="홈" />
@@ -49,7 +71,7 @@ const BookMemoPage = () => {
           <img src="/images/BookcaseIcon.svg" alt="책장" />
           <span>책장</span>
         </NavButton>
-        <NavButton active onClick={() => navigate("/note")}>
+        <NavButton active onClick={() => navigate("/bookmemo")}>
           <img src="/images/NoteIcon.svg" alt="노트" />
           <span>노트</span>
         </NavButton>
@@ -61,12 +83,12 @@ const BookMemoPage = () => {
 export default BookMemoPage;
 
 const Container = styled.div`
-  width: 100%;
-  height: 80vh;
-  background-color: #fdfdf8;
+  width: 393px;
+  height: 758px;
+  margin: 0 auto;
+  background-color: #FDFDF8;
   display: flex;
   flex-direction: column;
-  align-items: center;
   position: relative;
 `;
 
@@ -109,6 +131,7 @@ const SearchPlaceholder = styled.div`
 
 const MemoList = styled.div`
   width: 100%;
+  max-width: 430px;
   flex: 1;
   display: flex;
   flex-direction: column;
@@ -162,9 +185,14 @@ const MemoDate = styled.small`
   color: #777;
 `;
 
+const EmptyMessage = styled.p`
+  text-align: center;
+  color: #aaa;
+  margin-top: 50px;
+`;
+
 const BottomNav = styled.nav`
-  width: 100%;
-  max-width: 350px;
+  width: 393px;
   height: 60px;
   background-color: #f0edde;
   display: flex;
